82a83a9aa0405dae0c3f51ae5559176022213b39
blongden73 <hello@benlongdendesign.co.uk>
2016-07-20 10:03:15 +0100

    atom and headline changes

--- src/js/main.js
+++ src/js/main.js
@@ -29,20 +29,24 @@ function addBox() {
     var paragraph = document.querySelectorAll('.js-article__body>p')[9];
 
     var box = document.createElement('figure');
-    box.setAttribute('class', 'element element-interactive interactive element--supporting');
+    box.setAttribute('class', 'element element-interactive interactive element--supporting element--lab-atom');
     box.setAttribute('style', 'height: 100%;');
     box.innerHTML = '<div class="supporting-text"> <h1 class="supporting-text__header">What is Labour & Liverpool?</h1> <p class="supporting-text__content">This weekly series, led by Ewen MacAskill and guided by you, turns a spotlight on the Labour party. It is an experiment in collaborative reporting: we need you to contribute ideas, suggestions and feedback as Ewen visits Liverpool – bastion of Labour and lens through which we hope to understand the party – in the run-up to September’s party conference in the city. Weekly reports should keep you updated but not inundated.</p></div>';
 
     container.insertBefore(box, paragraph);
 }
 
+function addPartNumber() {
+
+}
+
 export function init(el, context, config, mediator) {
     if (document.querySelector('.content--labour-liverpool-article')) {
         removeLabourTagLink();
         addCutout();
-        for (var i = 0; i < document.getElementsByClassName('element-video').length; i++) {
-            addVideoStyle('element-video', i);
-        }
+        // for (var i = 0; i < document.getElementsByClassName('element-video').length; i++) {
+        //     addVideoStyle('element-video', i);
+        // }
         addBox();
     }
 }
